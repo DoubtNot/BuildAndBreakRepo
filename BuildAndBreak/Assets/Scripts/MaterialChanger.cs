fb180f98c9254554f0839af47bac60971bd0b0de
Spencer Anderson <lastof6.boyz@gmail.com>
2024-02-19 20:27:13 -0700

    TV Controller trigger buttons added. Flying fixed. hand animation added

--- BuildAndBreak/Assets/Scripts/MaterialChanger.cs
+++ BuildAndBreak/Assets/Scripts/MaterialChanger.cs
@@ -5,6 +5,16 @@ public class MaterialChanger : MonoBehaviour
     public Material newMaterial; // Assign the material you want to change to in the Unity Editor
     public GameObject targetObject; // Assign the target object in the Unity Editor
 
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the entering collider has the tag "Hand"
+        if (other.CompareTag("Hand"))
+        {
+            // Call the ChangeColor method when a collider with the "Hand" tag enters the trigger zone
+            ChangeColor();
+        }
+    }
+
     // Function to change the material of the specified object
     public void ChangeColor()
     {
